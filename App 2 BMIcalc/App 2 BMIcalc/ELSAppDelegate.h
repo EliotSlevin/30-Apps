1cd7c2114941cbe960181ca50aa30df0e298b6bf
Eliot Slevin <Eliot.slevin@gmail.com>
2014-02-02 15:37:27 +1300

    Added App 2

---
+++ App 2 BMIcalc/App 2 BMIcalc/ELSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ELSAppDelegate.h
+//  App 2 BMIcalc
+//
+//  Created by Eliot Slevin on 2/02/14.
+//  Copyright (c) 2014 Eliot Slevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App 2 BMIcalc/App 2 BMIcalcTests/App_2_BMIcalcTests.m
@@ -0,0 +1,34 @@
+//
+//  App_2_BMIcalcTests.m
+//  App 2 BMIcalcTests
+//
+//  Created by Eliot Slevin on 2/02/14.
+//  Copyright (c) 2014 Eliot Slevin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface App_2_BMIcalcTests : XCTestCase
+
+@end
+
+@implementation App_2_BMIcalcTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
